e53b2dfe0b81f715d8a1c8d8ab3978c44cd947ef
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-09-27 21:06:54 -0700

    Finished initial functions in sharkwords.js pre-event handling

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -20,13 +20,20 @@ const numWrong = 0;
 // Loop over the chars in `word` and create divs.
 // The divs should be appended to the section with id="word-container".
 const createDivsForChars = (word) => {
-  // Replace this with your code
+  for (const letter of word){
+    document.querySelector('#word-container').insertAdjacentHTML('beforeend',
+   `<div class="letter-box ${letter}"></div>`);
+  }
 };
 
 // Loop over each letter in the alphabet and generate a button for each letter
 // The buttons should be appended to the section with id="letter-buttons"
 const generateLetterButtons = () => {
-  // Replace this with your code
+  for (const letter of ALPHABET){
+    document.querySelector('#letter-buttons').insertAdjacentHTML('beforeend',
+    `<button class="letter-button">${letter}</button>`);
+  }
+
 };
 
 // Set the `disabled` property of `buttonEl` to `true.
@@ -34,7 +41,8 @@ const generateLetterButtons = () => {
 // `buttonEl` is an `HTMLElement` object.
 //
 const disableLetterButton = (buttonEl) => {
-  // Replace this with your code
+  // const buttonA = document.querySelector('button');
+  // buttonA.setAttribute('disabled', '');
 };
 
 // This is a helper function we will use in the future
@@ -42,7 +50,8 @@ const disableLetterButton = (buttonEl) => {
 // For now, you should test it out to make sure it works
 
 const isLetterInWord = (letter) => {
-  // Replace this with your code
+
+  return document.querySelectorAll(`.${letter}`).length > 0 ? true: null;
 };
 
 // This is like if __name__ == '__main__' in Python
@@ -53,11 +62,15 @@ const isLetterInWord = (letter) => {
   // You can change this to choose a random word from WORDS once you
   // finish this lab but we hard code it so we know what the word is
   // and can tell if things look correct for this word
+  // const word = WORDS[Math.floor(Math.random() * WORDS.length)];
   const word = 'hello';
-
   // call the function that makes an empty line for each letter in the word
   // Replace this line with the function call
-
+  createDivsForChars(word);
+  generateLetterButtons();
+  const buttonA = document.querySelector('button');
+  buttonA.setAttribute('disabled', '');
+  console.log(isLetterInWord('h'));
   // call the function that makes a button for each letter in the alphabet
   // Replace this line with the function call
 
